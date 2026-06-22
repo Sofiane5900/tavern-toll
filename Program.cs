cac57b3cf92f96494ba614ed68ce323f2871da00
Sofiane5900 <iguedjetalsofiane@gmail.com>
2026-06-22 13:56:08 +0200

    feat(hud): add basic stat bars to hud

--- Program.cs
+++ Program.cs
@@ -4,17 +4,25 @@ namespace HelloWorld;
 
 internal static class Program
 {
+
+    const int screenWidth = 800;
+    const int screenHeight = 450;
     // STAThread is required if you deploy using NativeAOT on Windows
     // See https://github.com/raylib-cs/raylib-cs/issues/301
     [System.STAThread]
     public static void Main()
     {
-        Raylib.InitWindow(800, 480, "Hello World");
+        Raylib.InitWindow(screenWidth, screenHeight, "Tavern's Toll");
 
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.White);
+            Raylib.ClearBackground(Color.Brown);
+
+            GameState gameState = new();
+            UIManager uiManager = new();
+
+            uiManager.DisplayBars(gameState, screenWidth);
 
             Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
 

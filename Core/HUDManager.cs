8981a3fbb1ae5c69aa6f511a228ff16c27a9fbe6
Sofiane5900 <iguedjetalsofiane@gmail.com>
2026-06-23 18:09:48 +0200

    feat: display hud in 540x960 rectangle screen

--- Core/HUDManager.cs
+++ Core/HUDManager.cs
@@ -4,31 +4,30 @@ using Raylib_cs;
 class HUDManager
 {
 
-
     public void DisplayBars(GameState gameState, int screenWidth)
     {
         int totalWidth = 0;
-        int spaceInBetween = 50;
+        int spaceInBetween = 25;
         int[] totalBars = new int[gameState.Bars.Count()];
         // Get the total width of all bars
         for (int i = 0; i < totalBars.Count(); i++)
         {
             string name = gameState.Bars[i].Name;
             int stats = gameState.Bars[i].CurrentStat;
-            totalBars[i] = Raylib.MeasureText($"{name}: {stats}", 20);
+            totalBars[i] = Raylib.MeasureText($"{name}: \n{stats}", 20);
             totalWidth += totalBars[i];
         }
         // 4 Bars in total (we don't need space after the last one)
         totalWidth += spaceInBetween * (totalBars.Count() - 1);
         int cursor = (screenWidth - totalWidth) / 2; // Position of the first bar
-        int Y = 50;
+        int Y = 200;
 
         // Draw all bars to the top of the screen
         for (int i = 0; i < totalBars.Count(); i++)
         {
             string name = gameState.Bars[i].Name;
             int stats = gameState.Bars[i].CurrentStat;
-            Raylib.DrawText($"{name}: {stats}", cursor, Y, 20, Color.White);
+            Raylib.DrawText($"{name}: \n{stats}", cursor, Y, 20, Color.White);
 
             // only if we aren't to the last element, keep incrementing cursor
             if (i < totalBars.Count() - 1) cursor += totalBars[i] + spaceInBetween;
@@ -54,4 +53,6 @@ class HUDManager
         Raylib.DrawTexturePro(gameState.CurrentCard.Avatar, cardRectangle, screen,
         cardCenter, 0, Color.White);
     }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,53 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 namespace HelloWorld;
 
 internal static class Program
 {
 
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+
+    const int totalScreenWidth = 1280;
+    const int totalScreenHeight = 720;
+
+
+    const int playableScreenWidth = 540;
+    const int playableScreenHeight = 960;
+
+
+
     // STAThread is required if you deploy using NativeAOT on Windows
     // See https://github.com/raylib-cs/raylib-cs/issues/301
     [System.STAThread]
     public static void Main()
     {
-        Raylib.InitWindow(screenWidth, screenHeight, "Tavern's Toll");
+        Raylib.InitWindow(totalScreenWidth, totalScreenHeight, "Tavern's Toll");
+
+
+        RenderTexture2D playableScreen = Raylib.LoadRenderTexture(playableScreenWidth, playableScreenHeight);
+        // Variables to draw our playableScreen 
+        Rectangle source = new(0, 0, playableScreenWidth, -playableScreenHeight);
+        Vector2 origin = new(playableScreenWidth / 2, playableScreenHeight / 2);
+        Rectangle dest = new(totalScreenWidth / 2, totalScreenHeight / 2, playableScreenWidth, playableScreenHeight);
+
+        GameState gameState = new();
+        HUDManager hudManager = new();
 
         while (!Raylib.WindowShouldClose())
         {
+            // Render HUD to framebuffer
+            Raylib.BeginTextureMode(playableScreen);
+            hudManager.DisplayBars(gameState, playableScreenWidth);
+            hudManager.DisplayCard(gameState, playableScreenWidth, playableScreenHeight);
+            Raylib.DrawRectangleLines(0, 0, playableScreenWidth, playableScreenHeight,
+Color.Black);
+            Raylib.EndTextureMode();
+
+            // Draw to the window 
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.Brown);
+            Raylib.DrawTexturePro(playableScreen.Texture, source, dest, origin, 0, Color.White);
 
-            GameState gameState = new();
-            HUDManager hudManager = new();
-
-            hudManager.DisplayBars(gameState, screenWidth);
-            hudManager.DisplayCard(gameState, screenWidth, screenHeight);
 
             Raylib.EndDrawing();
         }
